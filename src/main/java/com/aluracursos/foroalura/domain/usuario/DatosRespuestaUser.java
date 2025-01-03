f774853687a6cfa0a4ebfdf1d0270bfcad0c0f47
Leo <rbrian@academia.usbbog.edu.co>
2025-01-03 18:36:22 -0500

    :wrench: FIX: Unique response user.
    Modify response user to join on only response.

--- src/main/java/com/aluracursos/foroalura/domain/usuario/DatosRespuestaUser.java
+++ src/main/java/com/aluracursos/foroalura/domain/usuario/DatosRespuestaUser.java
@@ -2,13 +2,19 @@ package com.aluracursos.foroalura.domain.usuario;
 
 
 public record DatosRespuestaUser(
+        Long id,
         String nombre,
-        String email
+        String email,
+        String psswd,
+        String perfil
 ) {
     public DatosRespuestaUser(Usuario usuario) {
         this(
+                usuario.getId(),
                 usuario.getNombre(),
-                usuario.getEmail()
+                usuario.getEmail(),
+                usuario.getPassword(),
+                usuario.getPerfil().getNombre()
         );
     }
 }
